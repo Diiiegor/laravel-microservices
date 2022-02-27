793fe3d67a4dd740066cde893508de87c1d5c1cd
Juan Diego Oliveros <juandiegooliverosrios.d@gmail.com>
2022-02-27 18:52:09 -0500

    Api crud products

---
+++ admin/app/Http/Controllers/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController extends Controller
+{
+    public function index(){
+        return Product::all();
+    }
+
+    public function show($id){
+        return Product::query()->find($id);
+    }
+
+    public function store(Request $request){
+        $product = Product::query()->create($request->only('title','image'));
+        return response($product,Response::HTTP_ACCEPTED);
+    }
+
+    public function update($id,Request $request){
+        $product = Product::query()->find($id);
+        $product->update($request->only('title','image'));
+        return response($product,Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy($id){
+        Product::destroy($id);
+        return \response(null,Response::HTTP_NO_CONTENT);
+    }
+
+}
